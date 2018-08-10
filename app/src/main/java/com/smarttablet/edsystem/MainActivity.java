fb66d32eb5f8c847c0bc931828e170397d8a2412
AbydArs <abidr.w@gmail.com>
2018-08-10 15:37:59 +0500

    first commit

---
+++ app/src/main/java/com/smarttablet/edsystem/MainActivity.java
@@ -0,0 +1,33 @@
+package com.smarttablet.edsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView MyWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        MyWebView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = MyWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClient webViewClient = new WebViewClient();
+        MyWebView.setWebViewClient(webViewClient);
+
+        MyWebView.loadUrl("http://34.226.222.248:5000");
+    }
+}
